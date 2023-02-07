5dd8d4399f8e4d6a02b19fac368ea79b796453c3
Dylon Edwards <dthomas@gsitechnology.com>
2023-02-06 22:36:42 -0500

    Fixes common_api.c

--- src/gsi/common_api.c
+++ src/gsi/common_api.c
@@ -1,10 +1,14 @@
 #include "common_api.h"
 
+baryon_rsp32k_fifo_t rsp32k_fifo;
+baryon_rsp2k_fifo_t rsp2k_fifo;
 baryon_apuc_t apuc;
 baryon_seu_layer_t seu_layer;
 
 void baryon_init() {
-  baryon_init_apuc(&apuc);
+  baryon_init_rsp32k_fifo(&rsp32k_fifo);
+  baryon_init_rsp2k_fifo(&rsp2k_fifo);
+  baryon_init_apuc(&apuc, &rsp32k_fifo, &rsp2k_fifo);
   baryon_init_seu_layer(&seu_layer);
 }
 
--- src/gsi/common_api.h
+++ src/gsi/common_api.h
@@ -1,13 +1,24 @@
 #ifndef __GSI__BARYON__COMMON_API_H__
 #define __GSI__BARYON__COMMON_API_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "baryon/fifo.h"
 #include "baryon/apuc.h"
 #include "baryon/seu_layer.h"
 
+extern baryon_rsp32k_fifo_t rsp32k_fifo;
+extern baryon_rsp2k_fifo_t rsp2k_fifo;
 extern baryon_apuc_t apuc;
 extern baryon_seu_layer_t seu_layer;
 
 void baryon_init(void);
 void baryon_exit(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // __GSI__BARYON__COMMON_API_H__
